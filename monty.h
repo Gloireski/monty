de10b4bbe331d80b629b8c50fd7a4a7e0b18d4ac
Gloireski <35967799+Gloireski@users.noreply.github.com>
2023-10-19 16:35:50 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -3,6 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
 
 int main(int argc, char *argv[]);
 
